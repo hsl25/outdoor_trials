407fa10b404529b7fd3a13a69aa952b9e5ffb0a6
hsl25 <henocl205@gmail.com>
2026-03-24 22:02:22 +0000

    I made some progress on the calibration stage. I added functionality for the servo to increment the angle and add data to a buffer. The servo will sweep from 0 degrees to 180 degrees, then from 180 degrees back to 0 degrees. I still have to check this using the physical LiDAR and do some tests with the serial monitor. I also have to add code for updating the mean distance for each angle and each corresponding data point in the buffer.

--- outdoor_trials.cpp
+++ outdoor_trials.cpp
@@ -25,16 +25,66 @@ int main() {
     tof.device_setup();
 
     // TOF calibration
+    // This involves checking for errors and setting up timing budget
     tof.calibration();
 
     // Start continuous ranging
     // This function still needs to be modified for adding data into a buffer and calibrating
-    tof.read_tof_continuous();
+    // This is how calibration will run:
+    // 1. The servo will start at position 0 degrees
+    // 2. The servo will increment 1 degree. It will then indicate via one of something like: toggling a bit, raising a flag, etc. 
+    // 3. The LiDAR will then check if data is available, and if it is available, it will add it to the correct buffer position
+    // 4. After data has been successfully added, the servo will increment the angle again and the process will start again
 
-    // Create a buffer for calibration
-    // This creates a buffer with N = 10 data points
+    // Set up a flag to indicate when the servo has reached the end of the sweep
+    bool servo_end_flag = 0;
+
+    // Keep track of the number of sweeps the servo has done
+    int num_sweeps = 0;
+
+    // 1. Set the angle of the servo to 0 degrees - this is done in the for loop when i = 0
+    while (num_sweeps <= 10) {
+        while (!servo_end_flag) {
+            for (int i = 0; i < MAX_SERVO_ANGLE; i++) {
+                // Now increment the angle of the servo by 1 degree
+                servo.set_angle(i);
+
+                // When tof.read_continuous() runs, it checks whether data is available, and if so, it returns 1 data point
+                // Then, the data point is added to the buffer
+                // This is step 3
+                uint16_t lidar_data = tof.read_tof_continuous();
+                buffer.add_calib_sample(lidar_data); 
+
+                // Toggle the flag if the servo reaches the end of the sweep
+                if (i == MAX_SERVO_ANGLE) {
+                    servo_end_flag = !servo_end_flag; // servo_end_flag is equal to 1
+                }
+
+            }  
+        }
+
+        while (servo_end_flag) {
+            for (int i = MAX_SERVO_ANGLE; i > 0; i--) {
+                // Now increment the angle of the servo by 1 degree
+                servo.set_angle(i);
+
+                // When tof.read_continuous() runs, it checks whether data is available, and if so, it returns 1 data point
+                // Then, the data point is added to the buffer
+                // This is step 3
+                uint16_t lidar_data = tof.read_tof_continuous();
+                buffer.add_calib_sample(lidar_data); 
+
+                // Toggle the flag if the servo reaches the end of the sweep
+                if (i == 0) {
+                    servo_end_flag = !servo_end_flag;
+                }
+            } 
+        }
+        
+        num_sweeps++;
+
+    }
     
-    // 
 
     // Before the rover starts moving, scan the surroundings N times to get mean values for distances 
     // This is the initial calibration stage
