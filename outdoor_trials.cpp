2ef7430e09be97b078e2e6373dd46253355d6d86
hsl25 <henocl205@gmail.com>
2026-03-30 21:14:02 +0100

    Modified main for servo calibration by removing redundant while loops

--- outdoor_trials.cpp
+++ outdoor_trials.cpp
@@ -36,53 +36,43 @@ int main() {
     // 3. The LiDAR will then check if data is available, and if it is available, it will add it to the correct buffer position
     // 4. After data has been successfully added, the servo will increment the angle again and the process will start again
 
-    // Set up a flag to indicate when the servo has reached the end of the sweep
-    bool servo_end_flag = 0;
-
     // Keep track of the number of sweeps the servo has done
     int num_sweeps = 0;
 
     // 1. Set the angle of the servo to 0 degrees - this is done in the for loop when i = 0
     while (num_sweeps < CALIBRATION_SWEEPS) {
-        while (!servo_end_flag) {
-            for (int i = 0; i < MAX_SERVO_ANGLE; i++) {
-                // Now increment the angle of the servo by 1 degree
-                servo.set_angle(i);
+        for (int i = 0; i < MAX_SERVO_ANGLE; i++) {
+            // Now increment the angle of the servo by 1 degree
+            servo.set_angle(i);
 
-                // When tof.read_continuous() runs, it checks whether data is available, and if so, it returns 1 data point
-                // Then, the data point is added to the buffer
-                // This is step 3
-                uint16_t lidar_data = tof.read_tof_continuous();
-                // We do 2 * CALIBRATION_SWEEPS because each sweep passes through an angle twice
-                // I define 1 sweep as 0 -> 180 and 180 -> 0
-                float temp = ((float) lidar_data) / (2 * CALIBRATION_SWEEPS); 
-                buffer.add_calib_sample(temp, i); 
+            // Wait for servo to physically reach the degree
+            sleep_ms(15); 
 
-                // Toggle the flag if the servo reaches the end of the sweep
-                if (i == MAX_SERVO_ANGLE) {
-                    servo_end_flag = !servo_end_flag; // servo_end_flag is equal to 1
-                }
-
-            }  
+            // When tof.read_continuous() runs, it checks whether data is available, and if so, it returns 1 data point
+            // Then, the data point is added to the buffer
+            // This is step 3
+            uint16_t lidar_data = tof.read_tof_continuous();
+            // We do 2 * CALIBRATION_SWEEPS because each sweep passes through an angle twice
+            // I define 1 sweep as 0 -> 180 and 180 -> 0
+            float temp = ((float) lidar_data) / (2 * CALIBRATION_SWEEPS); 
+            buffer.add_calib_sample(temp, i); 
+  
         }
 
-        while (servo_end_flag) {
-            for (int i = MAX_SERVO_ANGLE; i > 0; i--) {
-                // Now increment the angle of the servo by 1 degree
-                servo.set_angle(i);
+        for (int i = MAX_SERVO_ANGLE; i > 0; i--) {
+            // Now increment the angle of the servo by 1 degree
+            servo.set_angle(i);
 
-                // When tof.read_continuous() runs, it checks whether data is available, and if so, it returns 1 data point
-                // Then, the data point is added to the buffer
-                // This is step 3
-                uint16_t lidar_data = tof.read_tof_continuous();
-                float temp = ((float) lidar_data) / (2 * CALIBRATION_SWEEPS);
-                buffer.add_calib_sample(temp, i);  
+            // Wait for servo to physically reach the degree
+            sleep_ms(15); 
 
-                // Toggle the flag if the servo reaches the end of the sweep
-                if (i == 0) {
-                    servo_end_flag = !servo_end_flag;
-                }
-            } 
+            // When tof.read_continuous() runs, it checks whether data is available, and if so, it returns 1 data point
+            // Then, the data point is added to the buffer
+            // This is step 3
+            uint16_t lidar_data = tof.read_tof_continuous();
+            float temp = ((float) lidar_data) / (2 * CALIBRATION_SWEEPS);
+            buffer.add_calib_sample(temp, i);  
+            
         }
         
         num_sweeps++;
